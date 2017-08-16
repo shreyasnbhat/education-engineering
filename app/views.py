ee96ef39ec1f1520b75ecd82e3afd5d3d41da1f7
Gautam Naik <gmn0105@gmail.com>
2017-08-16 14:57:34 +0530

    feat:Initialized SQLAlchemy DB
    fix:fixed minor splling error

--- app/views.py
+++ app/views.py
@@ -1,18 +1,25 @@
 from flask import render_template
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from app import app
+from db_models import Base, Student,engine
 
+'''Final Configuration'''
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 def getHomePage():
     return render_template('homepage.html')
 
 
-@app.route('/scores')
+@app.route('/users')
 def getScores():
-    return "<h1>Scores</h1>"
+    users = session.query(Student).all()
+    print users
+    return render_template('users.html', users = users)
 
 
-@app.route('/prediction')
+@app.route('/predictions')
 def getPredictions():
     return "<h1>Predictions</h1>"
