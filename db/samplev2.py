0a2b16bf30a0d067efa4d89c7a96d8c8b593d3c8
Gautam Naik <gmn0105@gmail.com>
2017-10-30 18:58:52 +0530

    Fix: File upload column update

--- db/samplev2.py
+++ db/samplev2.py
@@ -60,14 +60,18 @@ def generate_sample_db(path, course_id, course_name, db_session):
             db_session.rollback()
 
         # Add scores of the student
+        #If you want the entries to be displayed in order in DB Browser then use this otherwise comment it.
+        if(db_session.query(Score).filter_by(student_id = formatted_student_id).first() != None):
+            db_session.query(Score).filter_by(student_id = formatted_student_id, course_id = course_id).delete(synchronize_session = 'fetch')
+            db_session.commit()
+
         for mark_column in sample_mark_columns:
             score_name, score_max = mark_column.strip().split('-')
             score = sample_mark_frame.loc[i][mark_column]
-
             try:
                 db_session.add(Score(student_id=formatted_student_id,
-                                     name=score_name,
                                      course_id=course_id,
+                                     name=score_name,
                                      score=score))
                 db_session.commit()
             except IntegrityError:
